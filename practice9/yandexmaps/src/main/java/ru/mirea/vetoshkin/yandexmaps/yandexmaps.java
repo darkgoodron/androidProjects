ce3b02bdfaf50eb45380b21681b0fb11902fcf2e
goodron <lord_1998@mail.ru>
2022-05-03 17:24:39 +0300

    lab 9

---
+++ practice9/yandexmaps/src/main/java/ru/mirea/vetoshkin/yandexmaps/yandexmaps.java
@@ -0,0 +1,14 @@
+package ru.mirea.vetoshkin.yandexmaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class yandexmaps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yandexmaps);
+    }
+}
